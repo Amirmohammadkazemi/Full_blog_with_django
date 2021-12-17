9124f2d650568438bd9427d611988c6505c75bf9
Amirmohammad Kazemi <amirmohammadkazemi2002@gmail.com>
2021-12-17 13:22:10 +0330

    add pages view and url / connect to root

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request):
+    pass
+
+def AboutMe(request):
+    pass
+
+def Projects(request):
+    pass
+
+def Contact(request):
+    pass
+
+def Support(request):
+    pass
