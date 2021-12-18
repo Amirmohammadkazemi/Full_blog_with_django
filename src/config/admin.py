daced3d37b80c9ddd28b329d01eaf4626dd6649f
Amirmohammad Kazemi <amirmohammadkazemi2002@gmail.com>
2021-12-18 16:04:21 +0330

    set config admin and model

--- src/config/admin.py
+++ src/config/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from. import models
 
 # Register your models here.
+admin.site.register(models.Links)
+admin.site.register(models.Header)
+admin.site.register(models.Sub)
--- src/config/models.py
+++ src/config/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+
+class  Links(models.Model):
+    STATUS_CHOICES = (
+        ('Publish', 'انتشار'),
+        ('Draft', 'پیش نویس')
+    )
+    LinkName = models.CharField(max_length=100)
+    LinkAddress = models.CharField(max_length=1000, verbose_name='آدرس لینک')
+    LinkStatus = models.CharField(null=True, max_length=10, choices=STATUS_CHOICES, verbose_name='وضعیت')
+    
+class  Header(models.Model):
+    STATUS_CHOICES = (
+        ('Publish', 'انتشار'),
+        ('Draft', 'پیش نویس')
+    )
+    HeaderDescription = RichTextField()
+    HeaderStatus = models.CharField(null=True, max_length=10, choices=STATUS_CHOICES, verbose_name='وضعیت')
+    
+class  Sub(models.Model):
+    STATUS_CHOICES = (
+    ('Publish', 'انتشار'),
+    ('Draft', 'پیش نویس')
+    )
+    SubDescription = RichTextField()
+    SubStatus = models.CharField(null=True, max_length=10, choices=STATUS_CHOICES, verbose_name='وضعیت')
