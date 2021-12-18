e1db9245b28d49a4229c2749ccaff4ab9f07e4bc
Amirmohammad Kazemi <amirmohammadkazemi2002@gmail.com>
2021-12-18 12:36:36 +0330

    base.html added and config

--- src/root/settings.py
+++ src/root/settings.py
@@ -94,12 +94,6 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT =[
-    {
-        'DIRS': [
-            'static/'
-        ],
-    }
-]
+STATIC_ROOT = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
