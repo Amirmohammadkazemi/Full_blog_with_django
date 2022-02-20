5833ec1e84bb539e185c9ba26e14842e12d30076
AmirmohammadKazemi <amirmohammadkazemi2002@gmail.com>
2022-02-20 12:35:58 +0000

    admin panel actions

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -3,11 +3,20 @@ from . import models
 from .models import Article, Aboutme, Projects, Contact, Support, Category
 
 # Register your models here.
+def MakePublish(modeladmin, request, queryset):
+    update = queryset.update(ArticleStatus='Publish')
+MakePublish.short_description = "انتقال به منتشر شده ها"
+
+def MakeDraft(modeladmin, request, queryset):
+    queryset.update(ArticleStatus='Draft')
+MakeDraft.short_description = "انتقال به پیش نویس ها"
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('ArticleTitle', 'ArticleDate', 'ArticleStatus', 'category_name')
     list_filter = ('ArticleTitle', 'ArticleDate', 'ArticleStatus')
     search_fields = ('ArticleTitle', 'ArticleDate', 'ArticleStatus')
     ordering = ('-ArticleDate', '-ArticleStatus', 'ArticleTitle')
+    actions = [MakePublish, MakeDraft]
 
     def category_name(self, obj):
         return [category for category in obj.ArticleCategory.all()]
